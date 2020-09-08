3de4ac1987a49445f03843a83919171390edc8eb
Lefan Zhang <lefanz@amazon.com>
2020-09-08 15:53:44 +0000

    tests: new benchmarks

--- regression/abstraction/test.sh
+++ regression/abstraction/test.sh
@@ -27,7 +27,8 @@ AWS_C_COMMON_TESTS=(
     "aws_array_eq_c_str" 
     "aws_array_eq_c_str_ignore_case" 
     "aws_array_eq_ignore_case" 
-    "aws_byte_buf_eq"
+    "aws_byte_buf_eq" 
+    "aws_byte_buf_eq_ignore_case"
 )
 
 cwd=$(pwd)
