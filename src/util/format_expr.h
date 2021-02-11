20eab38b36a38667eee72643e5eb687d8c252f99
Daniel Kroening <dkr@amazon.com>
2021-02-11 20:09:44 +0000

    add_format_hook(...)

--- src/util/format_expr.h
+++ src/util/format_expr.h
@@ -10,11 +10,16 @@ Author: Daniel Kroening, kroening@kroening.com
 #define CPROVER_UTIL_FORMAT_EXPR_H
 
 #include "format.h"
+#include "irep.h"
+
+#include <functional>
 
 class exprt;
 
 //! Formats an expression in a generic syntax
 //! that is inspired by C/C++/Java, and is meant for debugging
 std::ostream &format_rec(std::ostream &, const exprt &);
+
+void add_format_hook(irep_idt, std::function<std::ostream &(std::ostream &, const exprt &)>);
 
 #endif // CPROVER_UTIL_FORMAT_EXPR_H
